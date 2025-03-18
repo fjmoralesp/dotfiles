7315c4f45532ace22381b0cc2c95ac6995ededd6
Francisco Morales <fjmoralesp@outlook.com>
2025-03-17 20:30:15 -0500

    feat: change terminal to kitty

--- install.ps1
+++
@@ -1,9 +0,0 @@
-$userPath = "C:\Users\angiz"
-$DOTFILES = Get-Location
-
-$destinationDirectory = Join-Path $userPath "AppData\Local\rio"
-If (Test-Path $destinationDirectory) {
-	Remove-Item -Path $destinationDirectory -Recurse -Force
-}
-
-Copy-Item -Path (Join-Path $DOTFILES "rio") -Destination $destinationDirectory -Recurse
