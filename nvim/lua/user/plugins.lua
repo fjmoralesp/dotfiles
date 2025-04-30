de561944e6897069a6211c0c285705eb5b6fd267
Francisco Morales <fjmoralesp@outlook.com>
2025-04-29 23:46:40 -0500

    feat: better search with leap

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -154,6 +154,14 @@ use({
   end,
 })
 
+use({
+  'ggandor/leap.nvim',
+  config = function()
+    require('leap').add_default_mappings()
+    vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
+  end,
+})
+
 -- File tree sidebar
 use({
   'nvim-neo-tree/neo-tree.nvim',
