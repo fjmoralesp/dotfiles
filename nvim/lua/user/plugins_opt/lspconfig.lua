3ca955832f937693bb6625cc33d056d6f410fa76
Francisco Morales <fjmoralesp@outlook.com>
2025-05-08 13:58:59 -0500

    fix: diagnostic deprecation

--- nvim/lua/user/plugins_opt/lspconfig.lua
+++ nvim/lua/user/plugins_opt/lspconfig.lua
@@ -39,9 +39,12 @@ vim.diagnostic.config({
     source = true,
   },
   underline = false,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '',
+      [vim.diagnostic.severity.WARN] = '',
+      [vim.diagnostic.severity.INFO] = '',
+      [vim.diagnostic.severity.HINT] = '',
+    },
+  },
 })
-
-vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
-vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
-vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
