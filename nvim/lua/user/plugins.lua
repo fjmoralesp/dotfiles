74962ba59c51bba15e2027c4855b7114ecbb2280
Francisco Morales <fjmoralesp@outlook.com>
2025-05-09 10:00:11 -0500

    feat: replace leap by flash

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -155,10 +155,15 @@ use({
 })
 
 use({
-  'ggandor/leap.nvim',
+  'folke/flash.nvim',
   config = function()
-    require('leap').add_default_mappings()
-    vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
+    require('flash').setup()
+    -- Key mappings
+    vim.keymap.set({ 'n', 'x', 'o' }, 's', function() require('flash').jump() end, { desc = 'Flash' })
+    vim.keymap.set({ 'n', 'x', 'o' }, 'S', function() require('flash').treesitter() end, { desc = 'Flash Treesitter' })
+    vim.keymap.set('o', 'r', function() require('flash').remote() end, { desc = 'Remote Flash' })
+    vim.keymap.set({ 'o', 'x' }, 'R', function() require('flash').treesitter_search() end, { desc = 'Treesitter Search' })
+    vim.keymap.set('c', '<c-s>', function() require('flash').toggle() end, { desc = 'Toggle Flash Search' })
   end,
 })
 
