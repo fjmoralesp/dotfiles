82c4f7ccf65d957aa7c07b6b5775c9430b29f6fc
Francisco Morales <fjmoralesp@outlook.com>
2025-05-11 14:33:49 -0500

    feat: update avante config

--- nvim/lua/user/plugins_opt/avante.lua
+++ nvim/lua/user/plugins_opt/avante.lua
@@ -1,25 +1,23 @@
-
 require('avante').setup({
-  ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
-  provider = "copilot", -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+  ---@alias Provider 'claude' | 'openai' | 'azure' | 'gemini' | 'cohere' | 'copilot' | string
+  provider = 'claude', -- The provider used in Aider mode or in the planning phase of Cursor Planning Mode
+  ---@alias Mode 'agentic' | 'legacy'
+  mode = 'agentic', -- The default mode for interaction. 'agentic' uses tools to automatically generate code, 'legacy' uses the old planning method to generate code.
   -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
   -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
   -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
-  auto_suggestions_provider = "copilot",
+  auto_suggestions_provider = 'claude',
   cursor_applying_provider = nil, -- The provider used in the applying phase of Cursor Planning Mode, defaults to nil, when nil uses Config.provider as the provider for the applying phase
-  copilot = {
-    endpoint = "https://api.githubcopilot.com",
-    model = "gpt-4o-2024-08-06",
-    proxy = nil, -- [protocol://]host[:port] Use this proxy
-    allow_insecure = false, -- Allow insecure server connections
-    timeout = 30000, -- Timeout in milliseconds
+  claude = {
+    endpoint = 'https://api.anthropic.com',
+    model = 'claude-3-5-sonnet-20241022',
     temperature = 0,
-    max_tokens = 20480,
+    max_tokens = 4096,
   },
   ---Specify the special dual_boost mode
   ---1. enabled: Whether to enable dual_boost mode. Default to false.
-  ---2. first_provider: The first provider to generate response. Default to "openai".
-  ---3. second_provider: The second provider to generate response. Default to "claude".
+  ---2. first_provider: The first provider to generate response. Default to 'openai'.
+  ---3. second_provider: The second provider to generate response. Default to 'claude'.
   ---4. prompt: The prompt to generate response based on the two reference outputs.
   ---5. timeout: Timeout in milliseconds. Default to 60000.
   ---How it works:
@@ -27,9 +25,9 @@ require('avante').setup({
   ---Note: This is an experimental feature and may not work as expected.
   dual_boost = {
     enabled = false,
-    first_provider = "openai",
-    second_provider = "claude",
-    prompt = "Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]",
+    first_provider = 'openai',
+    second_provider = 'claude',
+    prompt = 'Based on the two reference outputs below, generate a response that incorporates elements from both but reflects your own judgment and unique perspective. Do not provide any explanation, just give the response directly. Reference Output 1: [{{provider1_output}}], Reference Output 2: [{{provider2_output}}]',
     timeout = 60000, -- Timeout in milliseconds
   },
   behaviour = {
@@ -40,89 +38,88 @@ require('avante').setup({
     support_paste_from_clipboard = false,
     minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
     enable_token_counting = true, -- Whether to enable token counting. Default to true.
-    enable_cursor_planning_mode = false, -- Whether to enable Cursor Planning Mode. Default to false.
-    enable_claude_text_editor_tool_mode = false, -- Whether to enable Claude Text Editor Tool Mode.
   },
   mappings = {
     --- @class AvanteConflictMappings
     diff = {
-      ours = "co",
-      theirs = "ct",
-      all_theirs = "ca",
-      both = "cb",
-      cursor = "cc",
-      next = "]x",
-      prev = "[x",
+      ours = 'co',
+      theirs = 'ct',
+      all_theirs = 'ca',
+      both = 'cb',
+      cursor = 'cc',
+      next = ']x',
+      prev = '[x',
     },
     suggestion = {
-      accept = "<M-l>",
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
+      accept = '<M-l>',
+      next = '<M-]>',
+      prev = '<M-[>',
+      dismiss = '<C-]>',
     },
     jump = {
-      next = "]]",
-      prev = "[[",
+      next = ']]',
+      prev = '[[',
     },
     submit = {
-      normal = "<CR>",
-      insert = "<C-s>",
+      normal = '<CR>',
+      insert = '<C-s>',
     },
     cancel = {
-      normal = { "<C-c>", "<Esc>", "q" },
-      insert = { "<C-c>" },
+      normal = { '<C-c>', '<Esc>', 'q' },
+      insert = { '<C-c>' },
     },
     sidebar = {
-      apply_all = "A",
-      apply_cursor = "a",
-      retry_user_request = "r",
-      edit_user_request = "e",
-      switch_windows = "<Tab>",
-      reverse_switch_windows = "<S-Tab>",
-      remove_file = "d",
-      add_file = "@",
-      close = { "<Esc>", "q" },
-      close_from_input = nil, -- e.g., { normal = "<Esc>", insert = "<C-d>" }
+      apply_all = 'A',
+      apply_cursor = 'a',
+      retry_user_request = 'r',
+      edit_user_request = 'e',
+      switch_windows = '<Tab>',
+      reverse_switch_windows = '<S-Tab>',
+      remove_file = 'd',
+      add_file = '@',
+      close = { '<Esc>', 'q' },
+      close_from_input = nil, -- e.g., { normal = '<Esc>', insert = '<C-d>' }
     },
   },
   hints = { enabled = true },
   windows = {
-    ---@type "right" | "left" | "top" | "bottom"
-    position = "right", -- the position of the sidebar
+    ---@type 'right' | 'left' | 'top' | 'bottom'
+    position = 'right', -- the position of the sidebar
     wrap = true, -- similar to vim.o.wrap
-    width = 45, -- default % based on available width
-    height = 100,
+    width = 30, -- default % based on available width
     sidebar_header = {
       enabled = true, -- true, false to enable/disable the header
-      align = "center", -- left, center, right for title
-      rounded = false,
+      align = 'center', -- left, center, right for title
+      rounded = true,
     },
     input = {
-      prefix = "> ",
+      prefix = '> ',
       height = 8, -- Height of the input window in vertical layout
     },
     edit = {
+      border = 'rounded',
       start_insert = true, -- Start insert mode when opening the edit window
     },
     ask = {
       floating = false, -- Open the 'AvanteAsk' prompt in a floating window
       start_insert = true, -- Start insert mode when opening the ask window
-      ---@type "ours" | "theirs"
-      focus_on_apply = "ours", -- which diff to focus after applying
+      border = 'rounded',
+      ---@type 'ours' | 'theirs'
+      focus_on_apply = 'ours', -- which diff to focus after applying
     },
   },
   highlights = {
     ---@type AvanteConflictHighlights
     diff = {
-      current = "DiffText",
-      incoming = "DiffAdd",
+      current = 'DiffText',
+      incoming = 'DiffAdd',
     },
   },
   --- @class AvanteConflictUserConfig
   diff = {
     autojump = true,
     ---@type string | fun(): any
-    list_opener = "copen",
+    list_opener = 'copen',
     --- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
     --- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
     --- Disable by setting to -1.
