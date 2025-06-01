db84c45dc206b2392779e3df8bd5cb1b06b4246f
Francisco Morales <fjmoralesp@outlook.com>
2025-06-01 15:11:38 -0500

    fix: keymaps

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -5,9 +5,6 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
 
--- Paste replace visual selection without copying it.
-vim.keymap.set('v', 'p', 'P')
-
 -- Move text up and down
 vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
 vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
