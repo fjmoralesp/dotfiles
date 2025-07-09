bfa3d0a1f698d6f9ec4cc8836bceff5112f64520
Francisco Morales <fjmoralesp@outlook.com>
2025-07-09 00:27:17 -0500

    feat: vector code as mcp server

--- nvim/lua/user/plugins_opt/lspconfig.lua
+++ nvim/lua/user/plugins_opt/lspconfig.lua
@@ -25,10 +25,6 @@ vim.lsp.config("lua_ls", {
 			diagnostics = {
 				globals = { "vim" },
 			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
 			telemetry = {
 				enable = false,
 			},
