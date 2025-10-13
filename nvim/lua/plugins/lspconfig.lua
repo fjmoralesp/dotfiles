2864ac19d404d927ca92966a538ae1d43a0d65d8
Francisco Morales <fjmoralesp@outlook.com>
2025-10-13 13:59:44 -0500

    feat: kotlin lsp

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -5,21 +5,7 @@ return {
       jsonls = {},
       vtsls = {},
       pyright = {},
-      kotlin_language_server = {
-        filetypes = { "kotlin" },
-        root_markers = {
-          "settings.gradle", -- Gradle (multi-project)
-          "settings.gradle.kts", -- Gradle (multi-project)
-          "build.xml", -- Ant
-          "pom.xml", -- Maven
-          "build.gradle", -- Gradle
-          "build.gradle.kts", -- Gradle
-        },
-        cmd = { "kotlin-language-server" },
-        init_options = {
-          storagePath = os.getenv("HOME") .. "/.cache",
-        },
-      },
+      kotlin_lsp = {},
     },
   },
 }
