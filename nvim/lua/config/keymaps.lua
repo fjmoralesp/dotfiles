9efa3b0b85d31e45bd271c72758b0571d72b89fa
Francisco Morales <fjmoralesp@outlook.com>
2025-10-27 14:44:03 -0500

    feat: format json command

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -13,3 +13,6 @@ vim.keymap.set("v", "<leader>gf", ":OpenInGHFileLines<CR>")
 -- Code Companion
 vim.keymap.set({ "n", "v" }, "<leader>aa", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 vim.keymap.set({ "n", "v" }, "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+
+-- Format Json
+vim.keymap.set("v", "<leader>j", ":%!jq .<CR>")
