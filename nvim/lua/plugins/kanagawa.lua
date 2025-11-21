5c428cedc6ef726025279b69ee1d5786687d3f07
Francisco Morales <fjmoralesp@outlook.com>
2025-11-20 20:04:59 -0500

    fix: kanagawa borders

--- nvim/lua/plugins/kanagawa.lua
+++ nvim/lua/plugins/kanagawa.lua
@@ -8,6 +8,21 @@ return {
       undercurl = true,
       functionStyle = { bold = true, italic = true },
       dimInactive = true,
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          WinSeparator = { fg = "none", bg = theme.ui.nontext },
+        }
+      end,
       theme = "wave",
       background = {
         dark = "wave",
